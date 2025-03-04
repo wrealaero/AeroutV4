ccd161e2d04ccd5879f0ffe3938b00169617b7cc
wrealaero <clanwarobi@gmail.com>
2025-03-03 20:59:36 -0600

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,2 +1,2 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/ImDamc/VapeV4Reborn/refs/heads/main/main.lua", true))()
-queue_on_teleport('loadstring(game:HttpGet("https://raw.githubusercontent.com/ImDamc/VapeV4Reborn/refs/heads/main/main.lua", true))()')
+loadstring(game:HttpGet("https://raw.githubusercontent.com/wrealaero/AeroutV4/main/main.lua", true))()
+queue_on_teleport('loadstring(game:HttpGet("https://raw.githubusercontent.com/wrealaero/AeroutV4/main/main.lua", true))()')
