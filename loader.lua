da92189d7e4acacbb7aeb3b6bd4a9520cd06f855
wrealaero <clanwarobi@gmail.com>
2025-03-06 17:17:30 -0600

    Delete loader.lua

--- loader.lua
+++
@@ -1,2 +0,0 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/wrealaero/AeroutV4/main/main.lua", true))()
-queue_on_teleport('loadstring(game:HttpGet("https://raw.githubusercontent.com/wrealaero/AeroutV4/main/main.lua", true))()')
