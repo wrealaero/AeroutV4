25c5dc7c33ecc962090479acda2807e2def3e7e1
wrealaero <clanwarobi@gmail.com>
2025-03-09 18:22:51 -0500

    Delete newvape directory

--- newvape/libraries/XFunctions.lua
+++
@@ -1,9 +0,0 @@
---This watermark is used to delete the file if its cached, remove it to make the file persist after vape updates.
-local XFunctions = {}
-
-function XFunctions:SetGlobalData(key, value)
-    getgenv()[key] = value
-    shared[key] = value
-end
-
-return XFunctions
