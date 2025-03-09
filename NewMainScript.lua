072a04e54f1e509a9a3817e37059bfd5d844e056
wrealaero <clanwarobi@gmail.com>
2025-03-08 18:02:08 -0600

    Update NewMainScript.lua

--- NewMainScript.lua
+++ NewMainScript.lua
@@ -1,86 +1,45 @@
-local whitelistedUserIds = {
-    8113256672, 
-    7421025749,
-    1065346704
-}
-
-local function isPlayerWhitelisted(player)
-    for _, userId in pairs(whitelistedUserIds) do
-        if player.UserId == userId then
-            return true
-        end
-    end
-    return false
-end
-
-local player = game.Players.LocalPlayer
-
-if not isPlayerWhitelisted(player) then
-    warn(player.Name .. " is not whitelisted and cannot use the script.")
-    game:GetService("StarterGui"):SetCore("SendNotification", {
-        Title = "Access Denied";
-        Text = "You are not whitelisted to use this script.";
-        Duration = 5;
-    })
-    return
-end
-
 local isfile = isfile or function(file)
-    local suc, res = pcall(function()
-        return readfile(file)
-    end)
-    return suc and res ~= nil and res ~= ''
+	local suc, res = pcall(function()
+		return readfile(file)
+	end)
+	return suc and res ~= nil and res ~= ''
 end
 local delfile = delfile or function(file)
-    writefile(file, '')
+	writefile(file, '')
 end
 
 local function downloadFile(path, func)
-    if not isfile(path) then
-        local suc, res = pcall(function()
-            return game:HttpGet('https://raw.githubusercontent.com/miacheats/VapeV4ForRoblox/'..readfile('newvape/profiles/commit.txt')..'/'..select(1, path:gsub('newvape/', '')), true)
-        end)
-        if not suc or res == '404: Not Found' then
-            error(res)
-        end
-        if path:find('.lua') then
-            res = res
-        end
-        writefile(path, res)
-    end
-    return (func or readfile)(path)
+	if not isfile(path) then
+		local suc, res = pcall(function()
+			return game:HttpGet('https://raw.githubusercontent.com/QP-Offcial/VapeV4ForRoblox/'..readfile('newvape/profiles/commit.txt')..'/'..select(1, path:gsub('newvape/', '')), true)
+		end)
+		if not suc or res == '404: Not Found' then
+			error(res)
+		end
+		if path:find('.lua') then
+			res = '--This watermark is used to delete the file if its cached, remove it to make the file persist after vape updates.\n'..res
+		end
+		writefile(path, res)
+	end
+	return (func or readfile)(path)
 end
 
 local function wipeFolder(path)
-    if not isfolder(path) then return end
-    for _, file in listfiles(path) do
-        if file:find('loader') then continue end
-        if isfile(file) and select(1, readfile(file):find('--This watermark is used to delete the file if its cached, remove it to make the file persist after vape updates.')) == 1 then
-            delfile(file)
-        end
-    end
+	if not isfolder(path) then return end
+	for _, file in listfiles(path) do
+		if file:find('loader') then continue end
+		if isfile(file) and select(1, readfile(file):find('--This watermark is used to delete the file if its cached, remove it to make the file persist after vape updates.')) == 1 then
+			delfile(file)
+		end
+	end
 end
 
 for _, folder in {'newvape', 'newvape/games', 'newvape/profiles', 'newvape/assets', 'newvape/libraries', 'newvape/guis'} do
-    if not isfolder(folder) then
-        makefolder(folder)
-    end
+	if not isfolder(folder) then
+		makefolder(folder)
+	end
 end
 
-if not shared.VapeDeveloper then
-    local _, subbed = pcall(function()
-        return game:HttpGet('https://github.com/miacheats/VapeV4ForRoblox')
-    end)
-    local commit = subbed:find('currentOid')
-    commit = commit and subbed:sub(commit + 13, commit + 52) or nil
-    commit = commit and #commit == 40 and commit or 'main'
-    if commit == 'main' or (isfile('newvape/profiles/commit.txt') and readfile('newvape/profiles/commit.txt') or '') ~= commit then
-        wipeFolder('newvape')
-        wipeFolder('newvape/games')
-        wipeFolder('newvape/guis')
-        wipeFolder('newvape/libraries')
-    end
-    writefile('newvape/profiles/commit.txt', commit)
-end
+writefile('newvape/profiles/commit.txt', 'main')
 
 return loadstring(downloadFile('newvape/main.lua'), 'main')()
